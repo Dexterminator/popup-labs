a9a58e6e897622fb79d4cb6e76b6aca157ffe61d
Dexter Gramfors <dexter.gramfors@gmail.com>
2015-09-07 10:02:17 +0200

    Added skeleton files for intervalcover.

---
+++ lab1/intervalcover/src/se/dxtr/IntervalCover.java
@@ -0,0 +1,11 @@
+package se.dxtr;
+
+/**
+ * Created by dexter on 07/09/15.
+ */
+public class IntervalCover {
+
+    public static int[] intervalCover (int interval, int[] intervals) {
+        return null;
+    }
+}
