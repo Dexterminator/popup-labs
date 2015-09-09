8c866100291cb551d90107603b2fe25d883379a3
Dexter Gramfors <dexter.gramfors@gmail.com>
2015-09-09 14:41:49 +0200

    Probably correct length for lis, but no output sequence yet.

--- lab1/lis/src/se/dxtr/LIS.java
+++ lab1/lis/src/se/dxtr/LIS.java
@@ -1,10 +1,41 @@
 package se.dxtr;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * Created by dexter on 09/09/15.
  */
 public class LIS {
-    public static int[] lis (Object[] objects) {
-        return null;
+
+    public static final int NEGATIVE_INFINITY = Integer.MIN_VALUE;
+    public static final int INFINITY = Integer.MAX_VALUE;
+
+    public static List<Integer> lis (Integer[] objects) {
+        Integer[] last = new Integer[objects.length + 1];
+        last[0] = NEGATIVE_INFINITY;
+        for (int i = 1; i < last.length; i++)
+            last[i] = INFINITY;
+
+        for (int i = 0; i < objects.length; i++) {
+            int largestSmallerThanCurrent = NEGATIVE_INFINITY;
+            System.out.println ("Last for " + i + " " + Arrays.toString(last));
+            for (int l = 0; l <= i; l++) {
+                if (last[l] < objects[i] && l >= largestSmallerThanCurrent) {
+                    largestSmallerThanCurrent = l;
+                }
+            }
+            last[largestSmallerThanCurrent + 1] = Math.min (objects[i], last[largestSmallerThanCurrent + 1]);
+        }
+
+        System.out.println (Arrays.toString (last));
+        List<Integer> lis = new ArrayList<Integer> ();
+        for (int i = 1; i < last.length; i++) {
+            if (last[i] == INFINITY)
+                break;
+            lis.add (last[i]);
+        }
+        return lis;
     }
 }
--- lab1/lis/src/se/dxtr/Main.java
+++ lab1/lis/src/se/dxtr/Main.java
@@ -1,6 +1,7 @@
 package se.dxtr;
 
 import java.util.Arrays;
+import java.util.List;
 
 public class Main {
 
@@ -11,7 +12,9 @@ public class Main {
             Integer[] sequence = new Integer[length];
             for (int i = 0; i < length; i++)
                 sequence[i] = io.getInt ();
-            System.out.println (Arrays.toString (sequence));
+            System.out.println ("Input sequence: " + Arrays.toString (sequence));
+            List<Integer> lis = LIS.lis (sequence);
+            System.out.println (lis);
         }
     }
 }
