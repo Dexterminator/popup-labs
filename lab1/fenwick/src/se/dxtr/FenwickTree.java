4c72e9e46afc4e5832c7c138888c8d928c1f11ea
Dexter Gramfors <dexter.gramfors@gmail.com>
2015-09-14 12:13:33 +0200

    First test case ok for fenwick.

--- lab1/fenwick/src/se/dxtr/FenwickTree.java
+++ lab1/fenwick/src/se/dxtr/FenwickTree.java
@@ -7,13 +7,24 @@ public class FenwickTree {
     int[] treeSums;
 
     public FenwickTree (int n) {
-        treeSums = new int[n];
+        treeSums = new int[n+1];
     }
 
     public void add (int index, int delta) {
+        int n = index;
+        while (n < treeSums.length) {
+            treeSums[n] += delta;
+            n += (n & -n);
+        }
     }
 
     public int sum (int end) {
-        return 0;
+        int sum = 0;
+        int n = end;
+        while (n != 0) {
+            sum += treeSums[n];
+            n -= (n & -n);
+        }
+        return sum;
     }
 }
--- lab1/fenwick/src/se/dxtr/Main.java
+++ lab1/fenwick/src/se/dxtr/Main.java
@@ -3,6 +3,23 @@ package se.dxtr;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Kattio io = new Kattio (System.in, System.out);
+        int n = io.getInt ();
+        int q = io.getInt ();
+        FenwickTree fenwickTree = new FenwickTree (n);
+
+        for (int i = 0; i < q; i++) {
+            String operation = io.getWord ();
+            int index = io.getInt ();
+            if (operation.equals ("+")) {
+                int delta = io.getInt ();
+                fenwickTree.add (index, delta);
+            } else {
+                // Operation equals "?"
+                io.println (fenwickTree.sum (index));
+            }
+        }
+        io.flush ();
+        io.close ();
     }
 }
