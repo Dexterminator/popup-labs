7dd302691cd30b088f3392fc4c23ca5fa5b72dcc
Dexter Gramfors <dexter.gramfors@gmail.com>
2015-09-23 16:26:28 +0200

    Documented half of equationsolver.

--- lab1/equationsolver/src/se/ludjan/EquationSolver.java
+++ lab1/equationsolver/src/se/ludjan/EquationSolver.java
@@ -1,20 +1,28 @@
 package se.ludjan;
 
-import java.util.Arrays;
-
 /**
- * Created by Ludde on 15-09-22.
+ * Utility class for solving linear equations.
+ *
+ * Authors:
+ * Dexter Gramfors, Ludvig Jansson
  */
 public class EquationSolver {
     static final double EPSILON = 1e-10;
-    public static double[] solve(double[][] augmentedMatrix, int n){
-        double[] solution = new double[n];
+    enum State {INCONSISTENT, MULTIPLE}
+
+    /**
+     *
+     * @param augmentedMatrix the augmented matrix of equations and unknowns
+     * @param n the dimension of the system of equations, i.e the original matrix
+     * @return
+     */
+    public static double[] solve (double[][] augmentedMatrix, int n) {
         for (int i = 0; i < n; i++) {
             double[] currRow = augmentedMatrix[i];
-            if(Math.abs(currRow[i]) <= EPSILON){
+            if (Math.abs (currRow[i]) <= EPSILON) {
                 boolean singular = true;
-                for (int k = i+1; k < n; k++) {
-                    if(Math.abs(augmentedMatrix[k][i]) > EPSILON){
+                for (int k = i + 1; k < n; k++) {
+                    if (Math.abs (augmentedMatrix[k][i]) > EPSILON) {
                         // Try to disprove singularity, if so pivot these rows
                         singular = false;
                         double[] tempRow = augmentedMatrix[k];
@@ -24,25 +32,38 @@ public class EquationSolver {
                         break;
                     }
                 }
-                if(singular)
-                    return determineIfInsolvableOrMultiple(augmentedMatrix, n);   // singular
+                if (singular)
+                    return determineIfInsolvableOrMultiple (augmentedMatrix, n);
             }
-            for (int j = i+1; j < n; j++) {
-//                if(j == i)
-//                    continue;
-//                if(augmentedMatrix[j][i] == 0)
-//                    continue;
-                double times = augmentedMatrix[j][i]/currRow[i];
-                for (int k = 0; k < n + 1; k++) {
-                    // Subtract currRow from this row
-                    augmentedMatrix[j][k] -= times * currRow[k];
-                }
+            subtractRows (augmentedMatrix, n, i, currRow);
+        }
+
+        double[] solution = performBackSubstitution (augmentedMatrix, n);
+        return solution;
+    }
+
+    /**
+     * Subtract the correct multiple of currRow from the rows below it.
+     */
+    private static void subtractRows (double[][] augmentedMatrix, int n, int i, double[] currRow) {
+        for (int j = i + 1; j < n; j++) {
+            double times = augmentedMatrix[j][i] / currRow[i];
+            for (int k = 0; k < n + 1; k++) {
+                // Subtract currRow from this row
+                augmentedMatrix[j][k] -= times * currRow[k];
             }
         }
-        // Back substitution
-        for (int i = n-1; i >= 0; i--) {
+    }
+
+    /**
+     * Perform back substitution throughout the matrix, which is now in upper triangular form, thereby
+     * producing the solution.
+     */
+    private static double[] performBackSubstitution (double[][] augmentedMatrix, int n) {
+        double[] solution = new double[n];
+        for (int i = n - 1; i >= 0; i--) {
             double sum = 0.0;
-            for (int j = i+1; j < n; j++) {
+            for (int j = i + 1; j < n; j++) {
                 sum += augmentedMatrix[i][j] * solution[j];
             }
             solution[i] = (augmentedMatrix[i][n] - sum) / augmentedMatrix[i][i];
@@ -50,25 +71,26 @@ public class EquationSolver {
         return solution;
     }
 
-    public static double[] determineIfInsolvableOrMultiple(double[][] augmentedMatrix, int n){
-        double[] res = new double[n];
+    /**
+     *
+     */
+    private static State determineIfInsolvableOrMultiple (double[][] augmentedMatrix, int n) {
         for (int i = 0; i < n; i++) {
             boolean rowZero = true;
             for (int j = 0; j < n; j++) {
-                if(Math.abs(augmentedMatrix[i][j]) < EPSILON){
+                if (Math.abs (augmentedMatrix[i][j]) < EPSILON) {
                     continue;
                 } else {
                     rowZero = false;
                 }
             }
-            if(rowZero){
-                if (Math.abs(augmentedMatrix[i][n]) > EPSILON){
-                    return null;
+            if (rowZero) {
+                if (Math.abs (augmentedMatrix[i][n]) > EPSILON) {
+                    return State.INCONSISTENTt;
                 } else {
-                    return new double[]{Double.NaN};
+                    return State.MULTIPLE;
                 }
             }
         }
-        return res;
     }
 }
--- lab1/equationsolver/src/submit.sh
+++ lab1/equationsolver/src/submit.sh
@@ -1,1 +1,1 @@
-/Users/Ludde/python-custom/kattis.py -f -p equationsolver -m se.ludjan.Main se/ludjan/*.java
+kattis.py -f -p equationsolver -m se.ludjan.Main se/ludjan/*.java
