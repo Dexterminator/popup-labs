e31834fd2263e5ef115c3d765689bfc2950ee768
Ludvig Jansson <ludjan@kth.se>
2015-10-07 13:15:29 +0200

    Bellman-Ford done

--- lab2/lab2-assignments/src/se/dxtr/GraphLibrary/BellmanFord.java
+++ lab2/lab2-assignments/src/se/dxtr/GraphLibrary/BellmanFord.java
@@ -10,46 +10,38 @@ public class BellmanFord {
     public static DistanceResult shortestPath(Graph<Void, Weight> graph, Vertex<Void, Weight> start){
         long[] distance = new long[graph.getVertices ().size ()];
         Vertex[] parent = new Vertex[graph.getVertices ().size ()];
-        distance[start.getId ()] = 0;
 
         for (Vertex<Void, Weight> vertex : graph.getVertices ()) {
             if (vertex.getId () != start.getId ()) {
                 distance[vertex.getId ()] = Integer.MAX_VALUE;
             }
         }
-        
+        distance[start.getId ()] = 0;
+
         List<Vertex<Void, Weight>> vertexList = graph.getVertices();
         List<Edge<Void, Weight>> edgeList = graph.getEdges();
-        for (int i = 0; i < vertexList.size(); i++) {
-            int currID = vertexList.get(i).getId();
-            for (Edge<Void, Weight> edge : vertexList.get(currID).getEdges()) {
-                if(distance[edge.getFrom().getId()] + edge.getData().weight < distance[edge.getTo().getId()]){
+        for (int i = 0; i < vertexList.size()-1; i++) {
+            for (Edge<Void, Weight> edge : edgeList) {
+                if(distance[edge.getFrom().getId()] != Integer.MAX_VALUE && (edge.getData().weight + distance[edge.getFrom().getId()] < distance[edge.getTo().getId()])){
                     distance[edge.getTo().getId()] = distance[edge.getFrom().getId()] + edge.getData().weight;
                     parent[edge.getTo().getId()] = edge.getFrom();
                 }
             }
-//            for (Edge<Void, Weight> edge : edgeList) {
-//                if(distance[edge.getFrom().getId()] + edge.getData().weight < distance[edge.getTo().getId()]){
-//                    distance[edge.getTo().getId()] = distance[edge.getFrom().getId()] + edge.getData().weight;
-//                    parent[edge.getTo().getId()] = edge.getFrom();
-//                }
-//            }
         }
 
         for (Edge<Void, Weight> edge : edgeList) {
-            if(distance[edge.getFrom().getId()] + edge.getData().weight < distance[edge.getTo().getId()]) {
-                infect(graph, distance, edge.getTo().getId());
-                //distance[edge.getTo().getId()] = Integer.MIN_VALUE;
+            if(distance[edge.getFrom().getId()] != Integer.MAX_VALUE && distance[edge.getFrom().getId()] + edge.getData().weight < distance[edge.getTo().getId()]) {
+                propagateNegativeCycle(graph, distance, edge.getTo().getId());
             }
         }
         return new DistanceResult(distance, parent);
     }
 
-    public static void infect(Graph<Void, Weight> graph, long[] distance, int vertexId){
+    public static void propagateNegativeCycle(Graph<Void, Weight> graph, long[] distance, int vertexId){
         if(distance[vertexId] > Integer.MIN_VALUE){
             distance[vertexId] = Integer.MIN_VALUE;
             for (Edge<Void, Weight> edge : graph.getVertices().get(vertexId).getEdges()) {
-                infect(graph, distance, edge.getTo().getId());
+                propagateNegativeCycle(graph, distance, edge.getTo().getId());
             }
         }
 
