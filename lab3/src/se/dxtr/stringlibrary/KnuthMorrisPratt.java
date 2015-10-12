e30c76fa508c0b39fb6b1d9ab14a48bbff7e7633
Dexter Gramfors <dexter.gramfors@gmail.com>
2015-10-12 17:22:04 +0200

    Added lab3 project and string matching skeleton.

---
+++ lab3/src/se/dxtr/stringlibrary/KnuthMorrisPratt.java
@@ -0,0 +1,10 @@
+package se.dxtr.stringlibrary;
+
+import java.util.List;
+
+public class KnuthMorrisPratt {
+
+    public static List<Integer> findMatches (String pattern, String text) {
+        return null;
+    }
+}
---
+++ lab3/src/se/dxtr/stringmatching/Main.java
@@ -0,0 +1,19 @@
+package se.dxtr.stringmatching;
+
+import se.dxtr.stringlibrary.Kattio;
+import se.dxtr.stringlibrary.KnuthMorrisPratt;
+
+import java.util.List;
+
+public class Main {
+
+    static Kattio io = new Kattio (System.in, System.out);
+    public static void main(String[] args) {
+        while (io.hasMoreTokens ()) {
+            String pattern = io.getWord ();
+            String text = io.getWord ();
+            List<Integer> matchIndices = KnuthMorrisPratt.findMatches (pattern, text);
+        }
+        io.close ();
+    }
+}
