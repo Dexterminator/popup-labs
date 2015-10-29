5f511055affaaa0e5c18c1879b4f363a1c971805
Ludvig Jansson <ludjan@kth.se>
2015-10-29 14:14:24 +0100

    Added bufferedreader

--- lab3/src/se/dxtr/stringmatching/StringMatching.java
+++ lab3/src/se/dxtr/stringmatching/StringMatching.java
@@ -3,20 +3,29 @@ package se.dxtr.stringmatching;
 import se.dxtr.stringlibrary.Kattio;
 import se.dxtr.stringlibrary.KnuthMorrisPratt;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.List;
 import java.util.Scanner;
 
 public class StringMatching {
 
     static Kattio io = new Kattio (System.in, System.out);
-    static Scanner s = new Scanner(System.in);
+//    static Scanner s = new Scanner(new BufferedInputStream(System.in));
+    static BufferedReader s = new BufferedReader(new InputStreamReader(System.in));
+    static String pattern;
     public static void main(String[] args) {
-        while (s.hasNext()) {
-            String pattern = s.nextLine();
-            String text = s.nextLine();
-            List<Integer> matchIndices = KnuthMorrisPratt.findMatches (pattern, text);
-            matchIndices.forEach(i -> io.print(i + " "));
-            io.println ();
+        try {
+            while ((pattern = s.readLine()) != null) {
+                String text = s.readLine();
+                List<Integer> matchIndices = KnuthMorrisPratt.findMatches(pattern, text);
+                matchIndices.forEach(i -> io.print(i + " "));
+                io.println();
+            }
+        } catch (IOException e){
+            ;
         }
         io.close ();
     }
