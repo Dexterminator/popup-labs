d8c48ed8189b9db57ae6de56da5ee16932d2f497
Dexter Gramfors <dexter.gramfors@gmail.com>
2015-11-12 12:20:55 +0100

    Implemented add.

--- lab3/src/se/dxtr/numbertheorylibrary/RationalNumber.java
+++ lab3/src/se/dxtr/numbertheorylibrary/RationalNumber.java
@@ -4,5 +4,30 @@ package se.dxtr.numbertheorylibrary;
  * Created by dexter on 12/11/15.
  */
 public class RationalNumber {
-    
+    final public long numerator;
+    final public long denominator;
+
+    public RationalNumber(long numerator, long denominator) {
+        this.numerator = numerator;
+        this.denominator = denominator;
+    }
+
+    public RationalNumber add(RationalNumber other) {
+        long newDenominator = denominator * other.denominator;
+        long numerator1 = denominator * other.numerator;
+        long numerator2 = numerator * other.denominator;
+        long newNumerator = numerator1 + numerator2;
+        return shortestTerms(newNumerator, newDenominator);
+    }
+
+    private RationalNumber shortestTerms(long numerator, long denominator) {
+        long gcd = gcd(numerator, denominator);
+        return new RationalNumber(numerator / gcd, denominator / gcd);
+    }
+
+    private long gcd(long a, long b) {
+        if (b == 0)
+            return a;
+        return gcd(b, a % b);
+    }
 }
--- lab3/src/se/dxtr/rationalarithmetic/RationalArithmetic.java
+++ lab3/src/se/dxtr/rationalarithmetic/RationalArithmetic.java
@@ -1,5 +1,6 @@
 package se.dxtr.rationalarithmetic;
 
+import se.dxtr.numbertheorylibrary.RationalNumber;
 import se.dxtr.stringlibrary.Kattio;
 
 /**
@@ -9,6 +10,25 @@ public class RationalArithmetic {
 
     static Kattio io = new Kattio(System.in, System.out);
     public static void main(String[] args) {
+        int operations = io.getInt();
+        for (int i = 0; i < operations; i++) {
+            int x1 = io.getInt();
+            int y1 = io.getInt();
+            String op = io.getWord();
+            int x2 = io.getInt();
+            int y2 = io.getInt();
+            RationalNumber res = performOperation(new RationalNumber(x1, y1), new RationalNumber(x2, y2), op);
+            io.println(res.numerator + " / " + res.denominator);
+        }
+        io.close();
+    }
 
+    private static RationalNumber performOperation(RationalNumber num1, RationalNumber num2, String op) {
+        switch (op) {
+            case "+":
+                return num1.add(num2);
+            default:
+                return null;
+        }
     }
 }
