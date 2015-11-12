fbfa03ece530e34e975b128545b6169a43f41a43
Dexter Gramfors <dexter.gramfors@gmail.com>
2015-11-12 11:06:45 +0100

    Removed old code.

--- lab3/src/se/dxtr/stringlibrary/KnuthMorrisPratt.java
+++ lab3/src/se/dxtr/stringlibrary/KnuthMorrisPratt.java
@@ -30,30 +30,6 @@ public class KnuthMorrisPratt {
             }
         }
 
-
-
-        /*
-        if(chars.length == 1){
-            return new int[]{-1};
-        }
-        int pos = 2;
-        int currCandidateIndex = 0;
-        prefixTable[0] = -1;
-        prefixTable[1] = 0;
-
-        while (pos < pattern.length ()) {
-            if (chars[pos - 1] == chars[currCandidateIndex]) {
-                currCandidateIndex = currCandidateIndex + 1;
-                prefixTable[pos] = currCandidateIndex;
-                pos++;
-            } else if (currCandidateIndex > 0) {
-                currCandidateIndex = prefixTable[currCandidateIndex];
-            } else {
-                prefixTable[pos] = 0;
-                pos++;
-            }
-        }
-        */
         return prefixTable;
     }
 
@@ -78,25 +54,6 @@ public class KnuthMorrisPratt {
                     textPos++;
                 }
             }
-            /*
-            if(patternChars[currIndex] == textChars[matchStart+currIndex]){
-                if(currIndex == patternChars.length-1) {
-                    indices.add(matchStart);
-                    matchStart++;
-                    currIndex = 0;
-                } else {
-                    currIndex++;
-                }
-            } else {
-                matchStart += currIndex - prefixTable[currIndex];
-                if(prefixTable[currIndex] > -1) {
-                    currIndex = prefixTable[currIndex];
-                } else {
-                    currIndex = 0;
-//                    matchStart++;
-                }
-            }
-            */
         }
         return indices;
     }
